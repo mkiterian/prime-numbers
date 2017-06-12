41b00dcb775452053bb2800fb1ef4230aaa5d8e7
Mkiterian <hmukite@gmail.com>
2017-06-12 22:16:56 +0300

    added if statement to check if number is an integer

--- prime_numbers.py
+++ prime_numbers.py
@@ -5,29 +5,30 @@ def generate_prime_numbers(number):
 	tested = [1]
 
 	try:
-		#for every num from 1 to limit
-		if number > 0:
-			for num in range(1, number+1):
+		if isinstance(number, int):
+			#for every num from 1 to limit
+			if number > 0:
+				for num in range(1, number+1):
 
-				#create a list to hold factors of the num
-				factors = []
+					#create a list to hold factors of the num
+					factors = []
 
-				#check if num is divisible by x in the tested list
-				for i in range(len(tested)):
-					#if num is divisible by x, add x to the factors list 
-					if num % tested[i] == 0:
-						factors.append(tested[i])
+					#check if num is divisible by x in the tested list
+					for i in range(len(tested)):
+						#if num is divisible by x, add x to the factors list 
+						if num % tested[i] == 0:
+							factors.append(tested[i])
 
-				#add num to it's factors list
-				factors.append(num)
-				set_factors = set(factors)
-				tested.append(num)
+					#add num to it's factors list
+					factors.append(num)
+					set_factors = set(factors)
+					tested.append(num)
 
-				if len(set_factors) == 2:
-					primes.append(num)
+					if len(set_factors) == 2:
+						primes.append(num)
 
-			return primes
-		else:
-			return 'N should be a positive integer'
+				return primes
+			else:
+				return 'N should be a positive integer'
 	except TypeError:
 		raise TypeError
