59ec08542d24ce8f051eb8a409501b20bb8ff61e
Mkiterian <hmukite@gmail.com>
2017-06-12 15:40:54 +0300

    Added test function for correct return value given n is an integer

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -2,5 +2,15 @@ import unittest
 from prime_numbers import generate_prime_numbers
 
 class PrimeNumberTest(unittest.TestCase):
-	pass
+	
+	def test_generates_correct_prime_numbers(self):
+		#tests if function returns correct values given n is an integer
+		actual = generate_prime_numbers(10)
+		expected = [2,3,5,7]
+		self.assertEqual(actual, expected, msg='Expected [2,3,5,7] when n is 10')
+
+	
+
+if __name__ == '__main__':
+	unittest.main()
 
