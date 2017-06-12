1c9c9370d5d2d4a7a54e887ee81d6ad391015831
Mkiterian <hmukite@gmail.com>
2017-06-12 16:00:14 +0300

    added test to check if returned value is a list

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -14,6 +14,17 @@ class PrimeNumberTest(unittest.TestCase):
 		with self.assertRaises(TypeError, msg='n is not an integer'):
 			generate_prime_numbers('number')
 
+	def test_if_returned_value_is_a_list(self):
+		#check if number return is a list
+		self.assertIsInstance(generate_prime_numbers(10), list)
+
+	def test_if_number_of_numbers_is_correct(self):
+		#test if list returned has correct number 
+		actual = len(generate_prime_numbers(11))
+		expected = 5
+		self.assertEqual(actual, expected, msg='Number of returned items is not as expected')
+
+
 if __name__ == '__main__':
 	unittest.main()
 
