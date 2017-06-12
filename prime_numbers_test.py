63af95016bd7014c375dd4f39e698e0d847540d3
Mkiterian <hmukite@gmail.com>
2017-06-12 15:50:39 +0300

    Added test if n is an integer and gitignorefile

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -9,7 +9,10 @@ class PrimeNumberTest(unittest.TestCase):
 		expected = [2,3,5,7]
 		self.assertEqual(actual, expected, msg='Expected [2,3,5,7] when n is 10')
 
-	
+	def test_n_is_an_integer(self):
+		#tests if n is an integer 
+		with self.assertRaises(TypeError, msg='n is not an integer'):
+			generate_prime_numbers('number')
 
 if __name__ == '__main__':
 	unittest.main()
