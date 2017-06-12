5f4f01f7a4ddbfff14e8e15b4bb22a15b1da787e
Mkiterian <hmukite@gmail.com>
2017-06-12 17:45:31 +0300

    Refined readme and rearranged test cases

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Usage:
 3. ensure the latest python 3 is installed
 2. dependencies can be found in requirements.txt
 3. tests for the function are included in prime_numbers_test.py
-4. pass a positive integer into the generate_prime_numbers()
+4. call generate_prime_numbers() and pass a positive number(n)
+5. the returned value should be the list of prime numbers from 0 to n
--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -1,34 +1,34 @@
 import unittest
 from prime_numbers import generate_prime_numbers
 
-class PrimeNumberTest(unittest.TestCase):
-	
-	def test_generates_correct_prime_numbers(self):
-		#tests if function returns correct values given n is an integer
-		actual = generate_prime_numbers(10)
-		expected = [2,3,5,7]
-		self.assertEqual(actual, expected, msg='Expected [2,3,5,7] when n is 10')
+class PrimeNumberTest(unittest.TestCase):	
 
 	def test_n_is_an_integer(self):
 		#tests if n is an integer 
 		with self.assertRaises(TypeError, msg='n is not an integer'):
 			generate_prime_numbers('number')
 
+	def test_if_number_is_a_positive_integer(self):
+		#test if number is a positive integer
+		self.assertEqual(generate_prime_numbers(-10), 'N should be a positive integer', msg='Number Should be a positive integer')
+
 	def test_if_returned_value_is_a_list(self):
 		#check if number return is a list
 		self.assertIsInstance(generate_prime_numbers(10), list)
 
-	def test_if_number_of_numbers_is_correct(self):
+	def test_if_number_of_returned_numbers_is_correct(self):
 		#test if list returned has correct number 
 		actual = len(generate_prime_numbers(11))
 		expected = 5
 		self.assertEqual(actual, expected, msg='Number of returned items is not as expected')
 
-	def test_if_number_is_a_positive_integer(self):
-		#test if number is a positive integer
-		self.assertEqual(generate_prime_numbers(-10), 'N should be a positive integer', msg='Number Should be a positive integer')
+	def test_generates_correct_prime_numbers(self):
+		#tests if function returns correct values given n is an integer
+		actual = generate_prime_numbers(10)
+		expected = [2,3,5,7]
+		self.assertEqual(actual, expected, msg='Expected [2,3,5,7] when n is 10')
 
-
+	
 if __name__ == '__main__':
 	unittest.main()
 
